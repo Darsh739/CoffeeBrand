208221f3847e79bb5305a69523557f9649b5f91e
Darsh Garg <agca.svp2@gmail.com>
2026-04-10 14:48:41 +0530

    Change server port from 5000 to 10000

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: '*',}));
 app.use(express.json());
 
 // Product data
